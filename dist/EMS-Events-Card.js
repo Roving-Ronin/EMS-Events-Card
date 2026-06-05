4e37bf82a68599804d592841a9089ba536ff732d
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2026-06-05 13:05:01 +1000

    Delete dist/EMS-Events-Card.js

--- dist/EMS-Events-Card.js
+++
@@ -1,4 +0,0 @@
-// EMS-Events-Card - see haeo-events-card.js, emhass-events-card.js, energy-manager-card.js
-import "/hacsfiles/EMS-Events-Card/em-events-card.js";
-import "/hacsfiles/EMS-Events-Card/emhass-events-card.js";
-import "/hacsfiles/EMS-Events-Card/haeo-events-card.js";
